5e87b1b83f3479a5e986b7bc7b972d45f700ec48
Sachin Jaiswal <sachin.jaiswal@techdome.net.in>
2024-05-08 01:53:36 +0530

    answer given

--- program2.py
+++ program2.py
@@ -1,5 +1,12 @@
 def decode_message( s: str, p: str) -> bool:
-
-# write your code here
+        dp = [[False]*(len(p)+1) for i in range(len(s)+1)]
+        dp[0][0] = True
+        for j in range(1,len(p)+1):
+            if p[j-1] == '*':
+                dp[0][j] = dp[0][j-1]
+        
+        for i in range(1,len(s)+1):
+            for j in range(1,len(p)+1):
+                dp[i][j] = (p[j-1] in [s[i-1],'?','*'] and dp[i-1][j-1]) or (p[j-1] == '*' and (dp[i][j-1] or dp[i-1][j]))
   
-        return False
+        return dp[len(s)][len(p)]
